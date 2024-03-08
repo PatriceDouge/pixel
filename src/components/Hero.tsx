fa7317e652cfa69996d1e048529b7e69cea2e72d
Patrice Douge <patricedouge@gmail.com>
2024-03-08 11:48:12 -0500

    fixed logo component className issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Generate Pixel Art with AI
+        Generate pixel art from any image
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
       With pixel.jpg, you can generate pixel art from any image using AI.
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import CreoleProLogo from '@/images/logos/creole-pro-logo.png';
 
-export function Logo() {
+export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
     return (
         <h1>pixel.jpg</h1>
     )
