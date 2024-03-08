1938af263f343d059ae8b0cf874ebf514f47d391
Patrice Douge <patricedouge@gmail.com>
2024-03-07 23:17:37 -0500

    basic hero section

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+import CreoleProLogo from '@/images/logos/creole-pro-logo.png';
+
+export function Logo() {
+    return (
+        <h1>pixel.jpg</h1>
+    )
+  }
+  
